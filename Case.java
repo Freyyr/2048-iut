dcac7a1bc349164c9f1a40211f42dea474a7f851
imran <meghazi.imran@gmail.com>
2017-01-10 17:11:56 +0100

    fixing last bugs

--- Case.java
+++ Case.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 public class Case {
 
     private int value;
-    private Color color;
     private int x;
     private int y;
 
@@ -18,41 +17,6 @@ public class Case {
 	this.value = value;
 	this.x = x;
 	this.y = y;
-	switch(value){
-	case 2:
-	    this.color = new Color(0xc7de20);
-	    break;
-	case 4:
-	    this.color = new Color(0xdeb620);
-	    break;
-	case 8 :
-	    this.color = new Color(0xde7720);
-	    break;
-	case 16 :
-	    this.color = new Color(0xde3720);
-	    break;
-	case 32 :
-	    this.color = new Color(0xde2048);
-	    break;
-	case 64 :
-	    this.color = new Color(0xde2087);
-	    break;
-	case 128:
-	    this.color = new Color(0xde20c7);
-	    break;
-	case 256:
-	    this.color = new Color(0xb620de);
-	    break;
-	case 512:
-	    this.color = new Color(0x7720de);
-	    break;
-        case 1024:
-	    this.color = new Color(0x3720de);
-	    break;
-        case 2048:
-	    this.color = new Color(0x2048de);
-	    break;
-	}
     }
 
     public int getX(){
@@ -61,10 +25,6 @@ public class Case {
 
     public int getY(){
 	return this.y;
-    }
-
-    public Color getColor(){
-	return this.color;
     }
 
     public int getNum(){
--- Game.java
+++ Game.java
@@ -11,13 +11,18 @@ public class Game extends JPanel{
     private static int SIZE = 100;
     private static JFrame game = new JFrame();
     private static final int MARGE = 10;
+    private static final int width = 450;
+    private static final int height = 550;
     
     public Game(){
 	setFocusable(true);
 	addKeyListener(new KeyAdapter(){
 		@Override
 		public void keyPressed(KeyEvent e){
-		    if(! gameLost() && !gameWin()){
+		    if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			restart();
+		    }
+		    if(!gameLost() && !gameWin()){
 			switch(e.getKeyCode()){
 			case KeyEvent.VK_LEFT :
 			    moveLeft();
@@ -53,7 +58,10 @@ public class Game extends JPanel{
     public void paint(Graphics g){
 	super.paint(g);
 	g.setColor(new Color(0xbbada0));
-	g.fillRect(0, 0, 450, 450);
+	g.fillRect(0, 0, width, height);
+	g.setColor(Color.BLACK);
+	g.setFont(new Font("Arial", Font.BOLD, 20));
+	g.drawString("Appuyez sur Entrer pour relancer", SIZE/2 - MARGE, 80);
 	for(int y = 0; y < 4; y++){
 	    for(int x = 0; x < 4; x++){
 		draw(g, matrix[x][y], x, y);
@@ -64,35 +72,80 @@ public class Game extends JPanel{
     public void draw(Graphics g2, Case case1, int x, int y){
 	Graphics2D g = ((Graphics2D) g2);
 	if(case1 != null){
+	    //parametrage de l'affichage
 	    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 	    g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
 	    int num = case1.getNum();
 	    String value = Integer.toString(case1.getNum());
 
 	    //print case
-	    g.setColor(case1.getColor());
-	    g.fillRoundRect(x*SIZE+MARGE+x*MARGE, y*SIZE+MARGE+y*MARGE, SIZE, SIZE, MARGE, MARGE);
+	    g.setColor(getColor(case1.getNum()));
+	    g.fillRoundRect(x*SIZE+MARGE+x*MARGE, y*SIZE+MARGE+y*MARGE +100, SIZE, SIZE, MARGE, MARGE);
 
 	    //print int de la case
 	    g.setColor(Color.BLACK);
 	    g.setFont(stringFont);
 	    if(num < 10){
-		g.drawString(value, x*SIZE+MARGE+x*MARGE + 45 , y*SIZE+MARGE+y*MARGE + 55);
+		g.drawString(value, x*SIZE+MARGE+x*MARGE + 45 , y*SIZE+MARGE+y*MARGE + 155);
 	    }
 	    else if(num < 100){
-		g.drawString(value, x*SIZE+MARGE+x*MARGE + 36 , y*SIZE+MARGE+y*MARGE + 55);
+		g.drawString(value, x*SIZE+MARGE+x*MARGE + 38 , y*SIZE+MARGE+y*MARGE + 155);
 	    }
 	    else if(num < 1000){
-		g.drawString(value, x*SIZE+MARGE+x*MARGE + 32 , y*SIZE+MARGE+y*MARGE + 55);
+		g.drawString(value, x*SIZE+MARGE+x*MARGE + 31 , y*SIZE+MARGE+y*MARGE + 155);
 	    }
 	    else{
-		g.drawString(value, x*SIZE+MARGE+x*MARGE + 24 , y*SIZE+MARGE+y*MARGE + 55);
+		g.drawString(value, x*SIZE+MARGE+x*MARGE + 24 , y*SIZE+MARGE+y*MARGE + 155);
 	    }
 	}
 	else{
 	    g.setColor(Color.GRAY);
-	    g.fillRoundRect(x*SIZE+MARGE+MARGE*x, y*SIZE+MARGE+MARGE*y, SIZE, SIZE, MARGE, MARGE);
+	    g.fillRoundRect(x*SIZE+MARGE+MARGE*x, y*SIZE+MARGE+MARGE*y+100, SIZE, SIZE, MARGE, MARGE);
 	}
+	if(gameWin() || gameLost()){
+	    g.setColor(new Color(10, 10, 10, 30));
+	    g.fillRect(0, 0, width, height);
+	    g.setColor(Color.WHITE);
+	    g.setFont(new Font("Arial", Font.BOLD, 25));
+	    if(gameWin()){
+		g.drawString("Vous avez gagné !", SIZE, height/2);
+	    }
+	    else if(gameLost()){
+		g.drawString("Vous avez perdu !", SIZE, height/2);
+	    }
+	    g.setFont(new Font("Arial", Font.BOLD, 15));
+	    g.drawString("Appuyez la touche entrer pour relancer", SIZE- 3*MARGE + MARGE/4, height/2 + MARGE*2);
+	}
+    }
+
+    public Color getColor(int value) {
+	switch(value){
+	case 2:
+	    return new Color(0xc7de20);
+	case 4:
+	    return new Color(0xdeb620);
+	case 8 :
+	    return new Color(0xde7720);
+	case 16 :
+	    return new Color(0xde3720);
+	case 32 :
+	    return new Color(0xde2048);
+	case 64 :
+	    return new Color(0xde2087);
+	case 128:
+	    return new Color(0xde20c7);
+	case 256:
+	    return new Color(0xb620de);
+	case 512:
+	    return new Color(0x7720de);
+        case 1024:
+	    return new Color(0x3720de);
+        case 2048:
+	    return new Color(0x2048de);
+	default:
+	    break;
+	}
+	return Color.GRAY;
     }
     	
     public void moveLeft(){
@@ -403,7 +456,6 @@ public class Game extends JPanel{
 	    }
 	}
 	if(nbCaseVide > 0) return false;
-	System.out.println("Vous avez perdu !");
 	return true;
     }
     
@@ -412,7 +464,6 @@ public class Game extends JPanel{
 	for(int x = 0; x < 4; x++){ // Parcourt toutes les cases a la recherche d'une case de valeur 2048
 	    for(int y = 0; y < 4; y++){
 		if(this.matrix[x][y] != null && this.matrix[x][y].getNum() == 2048) {
-		    System.out.println("Vous avez gagné");
 		    return true;
 		}
 	    }
@@ -435,11 +486,10 @@ public class Game extends JPanel{
 	return new Case(number, x, y);
     }
 
-    
     public static void main(String args[]){
 	game.setTitle("2048 Game");
 	game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	game.setSize(450, 450);
+	game.setSize(width, height);
 	game.setResizable(false);
 
 	game.add(new Game());
